ea1cd5dcdcdfb11688aec1d21f300238612c3fda
kotek112 <kotek112lol@gmail.com>
2024-02-18 20:48:17 +0100

    usunięto pozostałości debugowania kodu

--- read.py
+++ read.py
@@ -21,16 +21,13 @@ def Alarm(log_time):
     GPIO.output(RELAY, GPIO.HIGH)
     GPIO.output(RED, GPIO.HIGH)
     while time.time() - log_time < 30:
-        print("prroz1")
         card = reader.read(1)
         time.sleep(0.1)
         if card is not None:
-            print("rozb")
             GPIO.output(RED, GPIO.LOW)
             id = card.value
             Check(id)
         time.sleep(1)
-    print("al")
     GPIO.output(RELAY, GPIO.LOW)
     GPIO.output(RED, GPIO.LOW)
     GPIO.cleanup()
@@ -39,25 +36,21 @@ def Disarm(login_time):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(GREEN, GPIO.OUT)
     GPIO.output(GREEN, GPIO.HIGH)
-    print('Disarmed')
     while time.time() - login_time < 15:
         if (time.time() - login_time > 10):
             GPIO.output(GREEN, GPIO.LOW)
         card = reader.read(1)
         time.sleep(0.1)
         if card is not None:
-            print("bl1")
             GPIO.output(GREEN, GPIO.HIGH)
             id = card.value
             login_time = time.time()
         time.sleep(1)
-    print("armed")
 
 def Check(id):
     payload = {"rfid": f'{id}'}
     response = requests.post(user_url,data = json.dumps(payload), headers = {'content-type':'application/ld+json'})
     if response.status_code == 422:
-        print('User not found. Alarm!')
         Alarm(log_time)
     elif response.status_code == 204:
         login_time = time.time()
@@ -74,25 +67,19 @@ while True:
     GPIO.setup(RED, GPIO.OUT)
     GPIO.output(GREEN, GPIO.LOW)
     GPIO.output(RED, GPIO.LOW)
-    print("b0")
     try:
-        print("b1")
         if GPIO.input(CZUJ):
-            print("b2")
             responseA = requests.get(locked_url)
             if responseA.json().get('locked'):
                 start_time = time.time()
-                print("r1")
                 card = reader.read(10)
                 time.sleep(0.1)
                 if card is not None:
-                    print("bl1")
                     id = card.value
                     login_time = time.time()
                     Disarm(login_time)
                     time.sleep(1)
                 else:
-                    print("blad")
                     log_time = time.time()
                     Alarm(log_time)
                 Check(id)
